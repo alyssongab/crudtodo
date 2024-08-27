e85ffd686027ad72979f21c59e572da494471993
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-08-27 17:04:28 -0400

    feat: animacao ease-in-out adicionada a div success

--- assets/scripts/script-signup.js
+++ assets/scripts/script-signup.js
@@ -3,9 +3,14 @@ function cadastrar(e) {
 
     const cadastroBloco = document.querySelector('.editable');
     const success = document.getElementById('success');
+    
     cadastroBloco.style.display = 'none';
-    success.style.display = 'block';
+    success.style.display = 'block'; // Torna o bloco visível para aplicar a animação
+    setTimeout(() => {
+        success.classList.add('show'); // Adiciona a classe que ativa a animação
+    }, 10); // Um pequeno atraso para garantir que a mudança de display tenha efeito
 }
 
 // Adiciona um ouvinte de eventos ao formulário para chamar a função cadastrar
 document.querySelector("form").addEventListener("submit", cadastrar);
+
