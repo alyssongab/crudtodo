fa2650339fba3413041e2113c6ae5a00d67ab495
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-08-30 18:49:03 -0400

    refactor: quebrando a função addTask em diferentes funções (incompleto)

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,12 +1,6 @@
-const newTask = document.getElementById("new-task");
-const warning = document.querySelector('.warning');
-
-const spanNoTask = document.querySelector('.no-task');
-const img = document.querySelector('.img-task');
-
-let taskIdCounter = 0;
-
 // ************************************************** //
+
+const newTask = document.getElementById("new-task");
 
 function logout() {
     window.alert("You have been logged out");
@@ -40,91 +34,110 @@ newTask.addEventListener('input', function() {
     }
 });
 
+let taskIdCounter = 0;
+const warning = document.querySelector('.warning');
+const spanNoTask = document.querySelector('.no-task');
+const img = document.querySelector('.img-task');
+
 function addTask() {
-    if(newTask.value === '' || newTask.value === 'Create new task...') {
+
+    const taskValue = newTask.value.trim();
+
+    if (!taskValue || taskValue === 'Create new task...') {
         warning.style.visibility = 'visible';
-    } else {
-        // incrementa o contador de tarefas (para a geração do id)
-        taskIdCounter++;
-    
-        // cria uma nova div
-        const newTaskDiv = document.createElement('div');
-
-        // 2 sub-divs: uma para checkbox e label e outra para as imagens;
-        const checkboxDiv = document.createElement('div');
-        newTaskDiv.appendChild(checkboxDiv);
-        checkboxDiv.className = 'checkbox-div';
-
-        const imgDiv = document.createElement('div');
-        imgDiv.className = 'img-box';
-        newTaskDiv.appendChild(imgDiv);
-
-        // Gerar um id para a div criada
-        newTaskDiv.id = `task-${taskIdCounter}`;
-
-        // Gera uma classe geral para cada div criada
-        newTaskDiv.classList.add('task-item');
-
-        // Novo input e label
-        const newInput = document.createElement('input');
-        newInput.type = 'checkbox';
-        newInput.className = 'checkbox-task';
-        checkboxDiv.appendChild(newInput);
-        
-        const newLabel = document.createElement('label');
-        newLabel.htmlFor = `task-${taskIdCounter}`;
-        newLabel.className = 'checkbox-label';
-        newLabel.innerHTML = newTask.value;
-        checkboxDiv.appendChild(newLabel);
-
-
-        // anexa a div ao container tasks
-        document.getElementById('tasks').appendChild(newTaskDiv);
-
-        // log para o backend
-        console.log('Tarefa criada. ID: ', newTaskDiv.id);
-
-        // Limpando o campo de input
-        newTask.value = 'Create new task...';
-        newTask.style.fontWeight = 'normal';
-
-        // Chama a função para limpar a div 'no-task'
-        styleNewTask();
-
-        // Imagens que serão um botão, para editar e remover tarefas
-        const imgEdit = document.createElement('img');
-        imgEdit.setAttribute('src', '../assets/img/edit-svg.svg');
-        imgEdit.setAttribute('alt', 'edit');
-        imgEdit.setAttribute('width', '40px');
-        imgEdit.setAttribute('height', '35px');
-        imgDiv.appendChild(imgEdit);
-
-        const imgRemove = document.createElement('img');
-        imgRemove.setAttribute('src', '../assets/img/remove-svg.svg');
-        imgRemove.setAttribute('alt', 'remove');
-        imgRemove.setAttribute('width', '40px');
-        imgRemove.setAttribute('height', '30px');
-        imgDiv.appendChild(imgRemove);
-
-        createButton(imgEdit, imgRemove);
-
-    }
-}
-
-
-function styleNewTask() {
+        return;
+    } 
 
     warning.style.visibility = 'hidden';
-    spanNoTask.remove();
-    img.remove();
+
+    // incrementa o contador de tarefas (para a geração do id)
+    taskIdCounter++;
+
+    // chama a função que cria uma nova div
+    const newTaskDiv = createTaskElement(taskValue, taskIdCounter);
+    document.getElementById('tasks').appendChild(newTaskDiv);
+
+    // Remove o span, a imagem e adiciona a classe 'has-task' ao bloco de tasks, que é tratado no css
+    if(spanNoTask) spanNoTask.remove();
+    if(img) img.remove();
     document.getElementById('tasks').classList.add('has-task');
-        
+
+    // Limpando o campo de input
+    newTask.value = 'Create new task...';
+    newTask.style.fontWeight = 'normal';
+
+    // log para o backend
+    console.log('Tarefa criada. ID: ', newTaskDiv.id);
+
+    // *********************************************************************************************************
+
+    // anexa a div ao container tasks
+    document.getElementById('tasks').appendChild(newTaskDiv);
+
+    // Chama a função para limpar a div 'no-task'
+    styleNewTask();
+
+    // Imagens que serão um botão, para editar e remover tarefas
+    const imgEdit = document.createElement('img');
+    imgEdit.setAttribute('src', '../assets/img/edit-svg.svg');
+    imgEdit.setAttribute('alt', 'edit');
+    imgEdit.setAttribute('width', '40px');
+    imgEdit.setAttribute('height', '35px');
+    imgDiv.appendChild(imgEdit);
+
+    const imgRemove = document.createElement('img');
+    imgRemove.setAttribute('src', '../assets/img/remove-svg.svg');
+    imgRemove.setAttribute('alt', 'remove');
+    imgRemove.setAttribute('width', '40px');
+    imgRemove.setAttribute('height', '30px');
+    imgDiv.appendChild(imgRemove);
+
+    // *********************************************************************************************************
+   
 }
 
-function createButton(imgEdit, imgRemove) {
-    const btn = document.createElement('button').className = 'bttn';
+// Função para criar a div que irá conter a task
 
-    btn.appendChild(imgEdit);
-    btn.appendChild(imgRemove);
+function createTaskElement(taskValue, taskId) {
+
+    const newTaskDiv = document.createElement('div');
+    newTaskDiv.id = `task-${taskId}`;
+    newTaskDiv.classList.add('task-item');
+
+    const newCheckbox = createCheckbox(taskValue, taskId);
+    const newImgDiv = createImgButtons();
+
+    newTaskDiv.appendChild(newCheckbox);
+    newTaskDiv.appendChild(newImgDiv);
+
+    return newTaskDiv;
+
+}
+
+
+// Função para criar a div do checkbox e o nome da tarefa
+
+function createCheckbox(taskValue, taskId) {
+    const checkboxDiv = document.createElemenet('div');
+    checkboxDiv.className = 'checkbox-div';
+
+    const newInput = document.createElement('input');
+    newInput.type = 'checkbox';
+    newInput.className = 'checkbox-task';
+
+    const newLabel = document.createElement('label');
+    newLabel.htmlFor = `task-${taskId}`;
+    newLabel.className = 'checkbox-label';
+    newLabel.textContent = taskValue;
+
+    checkboxDiv.appendChild(newInput);
+    checkboxDiv.appendChild(newLabel);
+
+    return checkboxDiv;
+}
+
+// Função para criar a div dos botões edit e remove
+
+function createImgButtons() {
 
 }
