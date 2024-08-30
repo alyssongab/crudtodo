445dde3dcd5fb76c2e9d3c7935914ebfa4bc356f
Alysson Gabriel <161105318+alyssongab@users.noreply.github.com>
2024-08-29 23:32:01 -0400

    input texto e botao melhorados. script de focus e blur adicionado

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,4 +1,18 @@
+// const newTask = document.getElementById("new-task");
+
 function logout() {
     window.alert("You have been logged out");
     window.location.href = "login.html";
 }
+
+document.getElementById("new-task").addEventListener('focus', function() {
+    if(this.value === 'Create new task...') {
+        this.value = '';
+    }
+});
+
+document.getElementById("new-task").addEventListener('blur', function() {
+    if(this.value === '') {
+        this.value = 'Create new task...';
+    }
+});
